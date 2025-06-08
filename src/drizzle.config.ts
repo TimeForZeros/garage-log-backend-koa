24237cf60820f51312eb7b6b7bd8a710a5483b76
Victor Gonzalez <dazedbutamused@gmail.com>
2025-06-08 13:23:38 -0700

    drizzle config update

--- src/drizzle.config.ts
+++ src/drizzle.config.ts
@@ -1,9 +1,8 @@
-import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  out: '../dist/drizzle',
-  schema: './db/schema.ts',
+  out: './dist/drizzle',
+  schema: './src/db/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
